a17b64cac2ae98c24934e69f9fb4ebfd61776d21
prameshk7 <pramesh.katuwal.7@gmail.com>
2024-07-05 18:10:33 +0545

    Eighth Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,26 @@ module.exports = {
       './MyInvest/investApp/templates/**/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        marquee:'marquee 10s linear infinite',
+        marquee2:'marquee 10s linear infinite',
+      },
+      keyframes:{
+        marquee:{
+          '0%' : {transform:'translateX(0%)'},
+          '100%' : {transform:'translateX(-100%)'},
+        },
+        marquee2:{
+          '0%' : {transform:'translateX(100%)'},
+          '100%' : {transform:'translateX(0%)'},
+          
+        },
+      },
+
+    },
   },
+  variants:{},
   plugins: [],
 
 
